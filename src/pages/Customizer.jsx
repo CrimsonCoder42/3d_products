8255da873e986bcb9ad8ceaa60caa84de227e5dd
CrimsonCoder42 <nostro37@gmail.com>
2023-08-15 12:55:46 -0400

    48:02

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -1,18 +1,178 @@
+// Importing necessary modules and components
 import React, { useState, useEffect } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
-import { useSnapshot } from 'valtio';
+import { AnimatePresence, motion } from 'framer-motion';  // For animations
+import { useSnapshot } from 'valtio';  // To use state
 
+// Configuration and utility imports
 import config from '../config/config';
-import state from '../store';
+import state from '../store';  // Global state
 import { download } from '../assets';
-import { downloadCanvasToImage, reader } from '../config/helpers';
-import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
-import { fadeAnimation, slideAnimation } from '../config/motion';
+import { downloadCanvasToImage, reader } from '../config/helpers';  // Helper functions
+import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';  // Constants
+import { fadeAnimation, slideAnimation } from '../config/motion';  // Animation configs
+
+// Component imports
 import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
 const Customizer = () => {
+  // Getting a snapshot of the global state
+  const snap = useSnapshot(state);
+
+  // Local states
+  const [file, setFile] = useState('');  // File state
+  const [prompt, setPrompt] = useState('');  // User input for AI
+  const [generatingImg, setGeneratingImg] = useState(false);  // Loading state for AI
+
+  const [activeEditorTab, setActiveEditorTab] = useState("");  // Active editor tab
+  const [activeFilterTab, setActiveFilterTab] = useState({  // Active filter tabs
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  // Generate appropriate content based on the selected editor tab
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />
+      case "filepicker":
+        return <FilePicker
+          file={file}
+          setFile={setFile}
+          readFile={readFile}
+        />
+      case "aipicker":
+        return <AIPicker 
+          prompt={prompt}
+          setPrompt={setPrompt}
+          generatingImg={generatingImg}
+          handleSubmit={handleSubmit}
+        />
+      default:
+        return null;
+    }
+  }
+
+  // Handle AI image generation and decal assignment
+  const handleSubmit = async (type) => {
+    if(!prompt) return alert("Please enter a prompt");
+    try {
+      setGeneratingImg(true);
+
+      const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          prompt,
+        })
+      })
+
+      const data = await response.json();
+      handleDecals(type, `data:image/png;base64,${data.photo}`)
+    } catch (error) {
+      alert(error)
+    } finally {
+      setGeneratingImg(false);
+      setActiveEditorTab("");
+    }
+  }
+
+  // Update global state based on decal type and result
+  const handleDecals = (type, result) => {
+    const decalType = DecalTypes[type];
+    state[decalType.stateProperty] = result;
+
+    if(!activeFilterTab[decalType.filterTab]) {
+      handleActiveFilterTab(decalType.filterTab)
+    }
+  }
+
+  // Toggle texture based on selected filter tab and update the state
+  const handleActiveFilterTab = (tabName) => {
+    switch (tabName) {
+      case "logoShirt":
+        state.isLogoTexture = !activeFilterTab[tabName];
+        break;
+      case "stylishShirt":
+        state.isFullTexture = !activeFilterTab[tabName];
+        break;
+      default:
+        state.isLogoTexture = true;
+        state.isFullTexture = false;
+        break;
+    }
+
+    setActiveFilterTab((prevState) => {
+      return {
+        ...prevState,
+        [tabName]: !prevState[tabName]
+      }
+    })
+  }
+
+  // Read the selected file and set its result as a decal
+  const readFile = (type) => {
+    reader(file)
+      .then((result) => {
+        handleDecals(type, result);
+        setActiveEditorTab("");
+      })
+  }
+
   return (
-    <div>Customizer</div>
+  <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key="custom"
+            className="absolute top-0 left-0 z-10"
+            {...slideAnimation('left')}
+          >
+            <div className="flex items-center min-h-screen">
+              <div className="editortabs-container tabs">
+                {EditorTabs.map((tab) => (
+                  <Tab 
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => setActiveEditorTab(tab.name)}
+                  />
+                ))}
+
+                {generateTabContent()}
+              </div>
+            </div>
+          </motion.div>
+
+          <motion.div
+            className="absolute z-10 top-5 right-5"
+            {...fadeAnimation}
+          >
+            <CustomButton 
+              type="filled"
+              title="Go Back"
+              handleClick={() => state.intro = true}
+              customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+            />
+          </motion.div>
+
+          <motion.div
+            className='filtertabs-container'
+            {...slideAnimation("up")}
+          >
+            {FilterTabs.map((tab) => (
+              <Tab
+                key={tab.name}
+                tab={tab}
+                isFilterTab
+                isActiveTab={activeFilterTab[tab.name]}
+                handleClick={() => handleActiveFilterTab(tab.name)}
+              />
+            ))}
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
   )
 }
 
